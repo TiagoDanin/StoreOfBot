849250e2a9f5920d5fbe1440721c2c8585d9c897
TiagoDanin <TiagoDanin@outlook.com>
2019-02-16 19:05:21 -0300

    feat: offline report

--- plugins/report.js
+++ plugins/report.js
@@ -1,27 +1,43 @@
+const databases = {
+	c: 'channels',
+	b: 'bots'
+}
+
 const base = async (ctx) => {
 	if (ctx.privilege <= 4) {
 		return
 	}
 
-	const id = ctx.match[2]
-	await ctx.database.del({id: id})
-	ctx.answerCbQuery('Removed!', true)
-	return
+	let status = 'Closed!'
+	if (ctx.match[2] == 'del') {
+		status = 'Removed!'
+		await ctx.database.del({id: Math.abs(ctx.match[3])}, databases[ctx.match[4]])
+	} else if (ctx.match[2] == 'off') {
+		status = 'Offline!'
+		await ctx.database.offline({id: Math.abs(ctx.match[3])}, databases[ctx.match[4]])
+	}
+
+	ctx.answerCbQuery(status, true)
+	return ctx.editMessageText(`${ctx.update.callback_query.message.text}\nStatus: ${status}`, {
+		parse_mode: 'HTML',
+		disable_web_page_preview: true
+	})
 }
 
 const send = async (ctx) => {
-	const id = Number(ctx.match[1])
-	let bot = await ctx.database.select({
-		id: id
-	})
-	if (!bot || bot.length <= 0) {
+	const id = Math.abs(Number(ctx.match[2]))
+	const database = databases[ctx.match[1]]
+	let db = await ctx.database.select({
+		id: Math.abs(id)
+	}, database)
+	if (!db || db.length <= 0) {
 		return
 	}
-	bot = bot[0]
+	db = db[0]
 	const text = `
 #Report by ${ctx.from.id} (${ctx.from.username})
-${bot.name} (${bot.username})
-${bot.description}
+${db.name} (${db.username}) AdminID ${db.admin}
+${db.description} Database ${database}
 	`
 
 	ctx.replyWithMarkdown('*Done!*')
@@ -29,7 +45,11 @@ ${bot.description}
 		reply_markup: {
 			inline_keyboard:
 			[
-				[{text: '✅ Remove', callback_data: `report:${bot.id}`}]
+				[{text: '❇️ Close Issue', callback_data: 'report:done'}],
+				[
+					{text: '✅ Remove', callback_data: `report:rem:${db.id}:${ctx.match[1]}`},
+					{text: '❕ Offline', callback_data: `report:off:${db.id}:${ctx.match[1]}`}
+				]
 			]
 		}
 	})
@@ -41,6 +61,6 @@ module.exports = {
 	plugin: send,
 	callback: base,
 	regex: [
-		/^\/report\s(\d*)$/i
+		/^\/report\s([bc])(\d*)$/i
 	]
 }
